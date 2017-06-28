4589cf5023f3813889b584dc4ce3b8975cead5c7
Yauhen_Kavalchuk <Yauhen_Kavalchuk@epam.com>
2017-06-28 16:48:31 +0300

    Added 'jsx' compilation. Added 'Divider' component. Updated lins in Header & Footer

--- src/containers/footer/footer.js
+++ src/containers/footer/footer.js
@@ -11,10 +11,14 @@ class Footer extends Component {
     return (
       <footer className="mdl-mini-footer">
         <div className="mdl-mini-footer__left-section">
-          <div className="mdl-logo">Title</div>
+          <div className="mdl-logo">e-Wallet</div>
           <ul className="mdl-mini-footer__link-list">
-            <li><a href="#">Help</a></li>
-            <li><a href="#">Privacy & Terms</a></li>
+            <li>
+              <a className="mdl-navigation__link mdl-navigation__link--icon" href="https://github.com/kovalchukevgeny/wallet">
+                <i className="material-icons">link</i>
+                <span>GitHub</span>
+              </a>
+            </li>
           </ul>
         </div>
       </footer>
--- src/containers/header/header.js
+++ src/containers/header/header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
+import Divider from './../../components/divider/divider.jsx';
 
 class Header extends Component {
   constructor(props) {
@@ -10,13 +11,30 @@ class Header extends Component {
   render() {
     return (
       <div className="demo-layout-transparent mdl-layout mdl-js-layout">
-        <header className="mdl-layout__header mdl-layout__header--transparent">
+        <header className="mdl-layout__header mdl-shadow--2dp">
           <div className="mdl-layout__header-row">
             {/* Title */}
             <span className="mdl-layout-title">e-Wallet</span>
             {/* Add spacer, to align navigation to the right */}
             <div className="mdl-layout-spacer"></div>
-
+            {/* search */}
+            <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable
+                  mdl-textfield--floating-label mdl-textfield--align-right">
+              <label
+                className="mdl-button mdl-js-button mdl-button--icon"
+                htmlFor="fixed-header-drawer-exp"
+              >
+                <i className="material-icons">search</i>
+              </label>
+              <div className="mdl-textfield__expandable-holder">
+                <input
+                  className="mdl-textfield__input"
+                  type="text"
+                  name="sample"
+                  id="fixed-header-drawer-exp"
+                />
+              </div>
+            </div>
           </div>
         </header>
         <div className="mdl-layout__drawer">
@@ -24,24 +42,34 @@ class Header extends Component {
           {/* Navigation */}
           <nav className="mdl-navigation">
             <Link
-              className="mdl-navigation__link"
-              to="/">
-              <span className="mdl-list__item">
-                <span className="mdl-list__item-primary-content">
-                  <i className="material-icons mdl-list__item-icon">home</i>
-                  Home
-                </span>
-              </span>
+              className="mdl-navigation__link mdl-navigation__link--icon"
+              to="/"
+            >
+              <i className="material-icons">home</i>
+              <span className="navigation__item">Home</span>
             </Link>
             <Link
-              className="mdl-navigation__link"
-              to="/">Link</Link>
+              className="mdl-navigation__link mdl-navigation__link--icon"
+              to="/categories"
+            >
+              <i className="material-icons">assignment_turned_in</i>
+              <span className="navigation__item">Categories</span>
+            </Link>
             <Link
-              className="mdl-navigation__link"
-              to="/">Link</Link>
+              className="mdl-navigation__link mdl-navigation__link--icon"
+              to="/transactions"
+            >
+              <i className="material-icons">account_balance_wallet</i>
+              <span className="navigation__item">Transactions</span>
+            </Link>
             <Link
-              className="mdl-navigation__link"
-              to="/">Link</Link>
+              className="mdl-navigation__link mdl-navigation__link--icon"
+              to="/results"
+            >
+              <i className="material-icons">timeline</i>
+              <span className="navigation__item">Results</span>
+            </Link>
+            <Divider />
           </nav>
         </div>
         <main className="mdl-layout__content">
