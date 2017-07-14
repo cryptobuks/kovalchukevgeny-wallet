13255d2f74c8f3e0deae73f2e3897b680e8e83f3
Yauhen_Kavalchuk <Yauhen_Kavalchuk@epam.com>
2017-07-14 15:45:01 +0300

    Added 'Panel' component

---
+++ src/components/panel/panel.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+class Panel extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    const { heading, footer, children, specialClass } = this.props;
+
+    return (
+      <div className={classNames('panel', specialClass)}>
+        {heading && <div className="panel-heading">
+          <h3 className="panel-title">{heading}</h3>
+        </div>}
+        <div className="panel-body">
+          {React.Children.map(children, (child) => {
+            return child;
+          })}
+        </div>
+        {footer && <div className="panel-footer">
+          <h3 className="panel-title panel-footer">{footer}</h3>
+        </div>}
+      </div>
+    );
+  }
+}
+
+Panel.defaultProps = {
+
+};
+
+Panel.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.element,
+    PropTypes.string
+  ]).isRequired,
+  heading: PropTypes.string,
+  footer: PropTypes.string
+};
+
+export default Panel;
