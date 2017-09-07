39961e6abce50688e3f2a79a16fd4714ef5bbdc6
Yauhen_Kavalchuk <Yauhen_Kavalchuk@epam.com>
2017-09-07 16:31:50 +0300

    Added new graph for 'CategoriesStats'

--- src/components/categories-stats/categories-stats.jsx
+++ src/components/categories-stats/categories-stats.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
+import { PieChart, Pie, Legend, Tooltip } from 'Recharts';
 
 import Icon from './../icon/icon.jsx';
 import Panel from './../panel/panel.jsx';
@@ -65,6 +66,20 @@ const CategoriesStats = props => {
     );
   });
 
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#8884d8', '#ed0cc1', '#f91616', '#ede944', '#827d7d'];
+  // Remap data for graph
+  let categoriesStatsPie = categoriesStats.map((categoryStats, index) => {
+    const categoryPercentage = Math.round((categoryStats.money / amountCategoryMoney) * 100);
+
+    COLORS[index] = COLORS[index] ? COLORS[index] : COLORS[Math.round(0 - 0.5 + Math.random() * (8 - 1 + 1))];
+    return {
+      id: index,
+      name: categoryStats.category,
+      value: categoryStats.money,
+      fill: COLORS[index]
+    }
+  });
+
   return (
     <div>
       {categories.length > 0 &&
@@ -82,6 +97,31 @@ const CategoriesStats = props => {
               {tableData}
             </div>
           </div>
+          <PieChart width={300} height={300}>
+            <Pie
+              data={categoriesStatsPie}
+              isAnimationActive={false}
+              dataKey="value"
+              cx={150}
+              cy={150}
+              innerRadius={50}
+              outerRadius={100}
+              fill="#8884d8"
+            >
+
+            </Pie>
+            <Legend
+              dataKey="money"
+              valueKey="category"
+              iconType='circle'
+              iconSize={10}
+              width={120}
+              height={140}
+              layout='vertical'
+              wrapperStyle={{top: 20, left: 320}}
+            />
+            <Tooltip />
+          </PieChart>
         </Panel>
       }
     </div>
