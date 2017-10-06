d8f91d29eeb73222fadff5a0bc09925780c9ada5
sergeitoptalo <toptalo@hotmail.com>
2017-10-06 14:13:31 +0300

    fix lint "space-before-function-paren" issues

--- tools/publicServer.js
+++ tools/publicServer.js
@@ -12,11 +12,11 @@ app.use(compression());
 app.use(express.static('public'));
 app.use(favicon(path.join(__dirname,'assets','public','favicon.ico')));
 
-app.get('*', function(req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.listen(port, function(err) {
+app.listen(port, function (err) {
   if (err) {
     console.log(err);
   } else {
