5c0a0336c6f5e8dbd545aeafc23109ca6db564ad
Yauhen_Kavalchuk <Yauhen_Kavalchuk@epam.com>
2017-10-17 13:14:14 +0300

    Rewrote actions to ES6 syntax

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -1,81 +1,63 @@
-export function addTransaction(id, date, money, description, category) {
-  return {
-    type: 'ADD_TRANSACTION',
-    id,
-    date,
-    money,
-    description,
-    category,
-  };
-}
+export const addTransaction = (id, date, money, description, category) => ({
+  type: 'ADD_TRANSACTION',
+  id,
+  date,
+  money,
+  description,
+  category,
+});
 
-export function changeTransaction(id, date, money, description, category) {
-  return {
-    type: 'CHANGE_TRANSACTION',
-    id,
-    date,
-    money,
-    description,
-    category,
-  };
-}
+export const changeTransaction = (id, date, money, description, category) => ({
+  type: 'CHANGE_TRANSACTION',
+  id,
+  date,
+  money,
+  description,
+  category,
+});
 
-export function deleteTransaction(transactionId) {
-  return {
-    type: 'DELETE_TRANSACTION',
-    transactionId,
-  };
-}
+export const deleteTransaction = transactionId => ({
+  type: 'DELETE_TRANSACTION',
+  transactionId,
+});
 
-export function addCategory(id, description, title, icon, filter, color) {
-  return {
-    type: 'ADD_CATEGORY',
-    id,
-    description,
-    title,
-    icon,
-    filter,
-    color,
-  };
-}
+export const addCategory = (id, description, title, icon, filter, color) => ({
+  type: 'ADD_CATEGORY',
+  id,
+  description,
+  title,
+  icon,
+  filter,
+  color,
+});
 
-export function updateCategory(id, description, title, icon, filter, color) {
-  return {
-    type: 'UPDATE_CATEGORY',
-    id,
-    description,
-    title,
-    icon,
-    filter,
-    color,
-  };
-}
+export const updateCategory = (id, description, title, icon, filter, color) => ({
+  type: 'UPDATE_CATEGORY',
+  id,
+  description,
+  title,
+  icon,
+  filter,
+  color,
+});
 
-export function changeAllCategories(categories) {
-  return {
-    type: 'CHANGE_ALL_CATEGORIES',
-    categories,
-  };
-}
+export const changeAllCategories = categories => ({
+  type: 'CHANGE_ALL_CATEGORIES',
+  categories,
+});
 
-export function deleteCategory(id) {
-  return {
-    type: 'DELETE_CATEGORY',
-    id,
-  };
-}
+export const deleteCategory = id => ({
+  type: 'DELETE_CATEGORY',
+  id,
+});
 
-export function changeLang(lang) {
-  return {
-    type: 'CHANGE_LANGUAGE',
-    lang,
-  };
-}
+export const changeLang = lang => ({
+  type: 'CHANGE_LANGUAGE',
+  lang,
+});
 
-export function addMonthCourse(date, course) {
-  return {
-    type: 'ADD_MONTH-COURSE',
-    date,
-    course,
-  };
-}
+export const addMonthCourse = (date, course) => ({
+  type: 'ADD_MONTH-COURSE',
+  date,
+  course,
+});
