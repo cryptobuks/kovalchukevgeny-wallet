dcece07fb22430af85bbe0331ff15344a635d4f5
Yauhen_Kavalchuk <Yauhen_Kavalchuk@epam.com>
2017-11-10 11:47:57 +0300

    'ErrorPage' component redesign

--- src/components/error-page/error-page.js
+++ src/components/error-page/error-page.js
@@ -6,13 +6,15 @@ import { Link } from 'react-router';
 import staticContent from './../../static-content/languages';
 
 const ErrorPage = props => {
-  const { lang } = props;
+  const { user } = props;
+  const { theme, lang } = user.settings;
+
   return (
-    <div className="not-found">
+    <div className={`not-found ${theme}`}>
       <div className="text">
-        <h1>{'404'}</h1>
+        <h1>404</h1>
         <h2>{staticContent[lang]['error-page'].h2}</h2>
-        <Link to="/" className="link">{staticContent[lang]['error-page'].link}</Link>
+        <Link to="/" className="btn btn-primary dark">{staticContent[lang]['error-page'].link}</Link>
       </div>
     </div>
   );
@@ -27,5 +29,5 @@ ErrorPage.propTypes = {
 };
 
 export default connect(state => ({
-  lang: state.lang,
+  user: state.user,
 }))(ErrorPage);
