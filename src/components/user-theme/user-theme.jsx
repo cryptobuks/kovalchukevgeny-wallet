e8951a23a299d894508003457e88aedd084dc17a
Yauhen_Kavalchuk <Yauhen_Kavalchuk@epam.com>
2017-11-16 12:58:09 +0300

    'UserTheme' component redesign

--- src/components/user-theme/user-theme.jsx
+++ src/components/user-theme/user-theme.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import Panel from './../../components/panel/panel.jsx';
 import Button from './../../components/button/button.jsx';
+import ButtonToolbar from './../../components/button-toolbar/button-toolbar.jsx';
 
 import staticContent from './../../static-content/languages';
 
@@ -29,16 +30,18 @@ class UserTheme extends Component {
         specialClass={`theme ${theme}`}
         heading={staticContent[lang]['user-setting'].theme}
       >
-        <Button
-          specialClass="btn btn-primary"
-          icon={'brightness_2'}
-          onClickFunction={() => this.changeTheme('dark')}
-        />
-        <Button
-          specialClass="btn btn-primary"
-          icon={'wb_sunny'}
-          onClickFunction={() => this.changeTheme('light')}
-        />
+        <ButtonToolbar>
+          <Button
+            specialClass="btn dark"
+            icon={'brightness_2'}
+            onClickFunction={() => this.changeTheme('dark')}
+          >{staticContent[lang]['user-setting'].themeDark}</Button>
+          <Button
+            specialClass="btn light"
+            icon={'wb_sunny'}
+            onClickFunction={() => this.changeTheme('light')}
+          >{staticContent[lang]['user-setting'].themeLight}</Button>
+        </ButtonToolbar>
       </Panel>
     );
   }
